385329e69ac4ed1bc4f0e85463868a71d9aa8690
Franko Vekić <franko.vekic@gmail.com>
2021-11-07 18:13:48 +0100

    Frontend mostly

--- cyberxgaming.sql
+++ cyberxgaming.sql
@@ -6,6 +6,7 @@ create table equipment(
 id int not null primary key auto_increment,
 name varchar(50) not null,
 price decimal(18,2) not null,
+smalldesc varchar(250),
 description text,
 quantity int not null,
 image varchar(50)
@@ -76,16 +77,16 @@ insert into user (email,password,name,surname,role) values
 ('admin@gmail.com','$2y$10$WHV1bOXJTbMzrtZEIWO97.2ycbapSP0JweaAC1iP5luFC9wosSsk2','Admin','Test','admin'),
 ('oper@gmail.com','$2y$10$WHV1bOXJTbMzrtZEIWO97.2ycbapSP0JweaAC1iP5luFC9wosSsk2','Operater','Test','oper');
 
-insert into equipment (name,price,description,quantity,image) values 
-('Razer Lachesis',79.99,'The Razer Lachesis reigns supreme with a true 5600dpi 3.5G Laser sensor, which 
+insert into equipment (name,price,smalldesc,description,quantity,image) values 
+('Razer Lachesis',79.99,'The Razer Lachesis reigns supreme with a true 5600dpi 3.5G Laser sensor.','The Razer Lachesis reigns supreme with a true 5600dpi 3.5G Laser sensor, which 
 enables movement speeds of 7 times that of a standard 800dpi optical sensor. Customize the look of the Razer 
 Lachesis however you want. With the new Tricolor-LED you get a unique look & style on your favourite gaming weapon.',10,'razerlachesis.jpg'),
 
-('Razer Nari',80,'The wireless gaming headset designed for supreme comfort. Featuring an auto-adjusting headband, with swiveling 
+('Razer Nari',80,'The wireless gaming headset designed for supreme comfort.','The wireless gaming headset designed for supreme comfort. Featuring an auto-adjusting headband, with swiveling 
 earcups that include cooling gel-infused ear cushions. THX Spatial Audio provides next-generation surround sound and the game/chat 
 balance allows the perfect mix of game and chat volume.',10,'razernari.jpg'),
 
-('Red CyberX gaming chair',100,'A cyberx gaming chair is a type of chair designed for the comfort of gamers. They differ from most office chairs
+('Red CyberX chair',100,'A cyberx gaming chair is a type of chair designed for the comfort of gamers.','A cyberx gaming chair is a type of chair designed for the comfort of gamers. They differ from most office chairs
  in having high backrest designed to support the upper back and shoulders. They are also more customizable: the armrests, back, lumbar support 
 and headrest can all be adjusted for comfort and efficiency.',10,'redcyberxchair.jpg');
 
